2f50c0974b53d23e4d498fecc09fe209d97f0ccf
JARS3N <JARS3N@users.noreply.github.com>
2018-02-06 14:53:37 -0500

    Create get_coefs.R

---
+++ R/get_coefs.R
@@ -0,0 +1,12 @@
+get_coefs<-function(BMID){
+  require(RMySQL)
+  db<-adminKraken::con_mysql()
+  q_1<-'SELECT PH_A, PH_B, PH_C, O2_A, O2_B,CONCAT(substr(`Sensor 2 C1`, 1, 4),"e",substr(`Sensor 2 C1`, 5, 6)) AS BF from barcodematrixview where ID = '
+  q_3<-";"
+  query_string<-paste0(q_1,BMID,q_3)
+  query_sent<-suppressWarnings(RMySQL::dbSendQuery(db,query_string))
+  x<-RMySQL::dbFetch(query_sent)
+  RMySQL::dbClearResult(query_sent)
+  RMySQL::dbDisconnect(db)
+  x
+}
