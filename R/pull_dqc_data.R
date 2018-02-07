17fea0bb94080a3a9dba4e75ec92ce846244fc46
JARS3N <JARS3N@users.noreply.github.com>
2018-02-07 16:27:49 -0500

    Create pull_dqc_data.R

---
+++ R/pull_dqc_data.R
@@ -0,0 +1,14 @@
+pull_dqc_data<-function(Lot,var){
+  db <-adminKraken::con_mysql()
+  q_1 <-"SELECT sn,"
+  q_2 <-' as var FROM dryqcxf24 where Lot = "'
+  q_3 <-'";'
+  query_string<-paste0(q_1,var,q_2,Lot,q_3)
+  send_query<-RMySQL::dbSendQuery(db,query_string)
+  fetch_query<-RMySQL::dbFetch(send_query)
+  RMySQL::dbClearResult(send_query)
+  RMySQL::dbDisconnect(db)
+  fetch_query
+  fetch_query$sn<-factor(as.numeric(fetch_query$sn))
+  fetch_query
+}
