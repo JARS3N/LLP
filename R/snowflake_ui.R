1c7196df15446c86d3ebb67733651ff4602d00e7
JARS3N <JARS3N@users.noreply.github.com>
2018-02-07 16:51:22 -0500

    Create snowflake_ui.R

---
+++ R/snowflake_ui.R
@@ -0,0 +1,16 @@
+snowfalke_ui <- function() {
+  require(shiny)
+  shinyUI(fluidPage(
+    titlePanel("SnowFlake"),
+    # Sidebar with a slider input for number of bins
+    sidebarLayout(
+      sidebarPanel(
+        shiny::textInput("user", "user", value = " "),
+        shiny::textInput("comments", "comments", value = " "),
+        downloadButton("export", "export"),
+        width = 2
+      ),
+      mainPanel(uiOutput("data_input"))
+    )
+  ))
+}
