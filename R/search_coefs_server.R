e02bac7c3b0f8af59da9321998540b7dceee76dc
JARS3N <JARS3N@users.noreply.github.com>
2018-02-07 11:22:07 -0500

    Update search_coefs_server.R

--- R/search_coefs_server.R
+++ R/search_coefs_server.R
@@ -21,7 +21,7 @@ shinyServer(function(input, output, session) {
     output$pHtable <- renderTable(select(info, contains('O2')) %>%
                                     mutate(O2_A = as.character(round(O2_A, 0))),
                                   digits = 6)
-    if(as.numeric(BF)<0.0001){
+    if(info$BF==0){
        output$bftbl <<- NULL
       }else{
     output$bftbl <<-
