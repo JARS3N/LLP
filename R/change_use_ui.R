ea01660d2fdbca032b69a7ffd39d79a85443073a
JARS3N <JARS3N@users.noreply.github.com>
2018-02-09 13:28:18 -0500

    Create change_use_ui.R

---
+++ R/change_use_ui.R
@@ -0,0 +1,22 @@
+change_use_ui<-function(){
+require(shiny)
+shinyUI(fluidPage(
+  titlePanel("Adjust Use Number"),
+  sidebarLayout(
+    sidebarPanel(
+      numericInput('n','newUseNumber',value=0,min=-9999,max=999,step=1),
+      fileInput("file1", "upload CartridgeBarcodes.XML",
+                accept = c(
+                  "CartridgeBarcodes.XML",
+                  ".XML")
+      ),
+      downloadButton('downloadData', 'Download')
+    ),
+    mainPanel(
+      p('Upload file "CartridgeBarcodes.XML"'),
+      p('File should be located @ C:\\ProgramData\\Seahorse Bioscience, Inc\\Seahorse Wave\\Data\\Cartridges'),
+      p('Replace original file with new file & rename as "CartridgeBarcodes.XML"')
+    )
+  )
+)
+}
