4da0fabe9c08f152cdba27e9219de0a34e2c3865
JARS3N <JARS3N@users.noreply.github.com>
2018-02-09 15:36:57 -0500

    Update pull_mvdata.R

--- R/pull_mvdata.R
+++ R/pull_mvdata.R
@@ -1,6 +1,6 @@
  pull_mvdata<-function(platform,var){
     db<-adminKraken::con_mysql()
-    q<-'SELECT Lot,%var% as var,RIGHT(Lot,2) as yr,SUBSTR(Lot,2,4) as day,substr(LOT,2,1)="E" as exp from mvdata where plat="%plat%" order by yr ASC, day ASC;'
+    q<-'SELECT Lot,%var% as var,RIGHT(Lot,2) as yr,SUBSTR(Lot,2,3) as day,substr(LOT,2,1)="E" as exp from mvdata where plat="%plat%" order by yr ASC, day ASC;'
     q_str<-gsub("%plat%",platform,gsub("%var%",var,q))
     send_query<-RMySQL::dbSendQuery(db,q_str)
     fetch_query<-RMySQL::dbFetch(send_query,n=-1)
