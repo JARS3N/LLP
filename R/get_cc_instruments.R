d83c556622fa4b5c445570fde87bbf2245bfd0a7
JARS3N <JARS3N@users.noreply.github.com>
2018-02-11 00:43:15 -0500

    Update get_cc_instruments.R

--- R/get_cc_instruments.R
+++ R/get_cc_instruments.R
@@ -1,9 +1,10 @@
 get_cc_instruments<-function(u){
 require(RMySQL)
 db<-adminKraken::con_mysql()
-query_string<-gsub("%tbl%",u,'SELECT DISTINCT(INST) from %tbl%;')
+query_string<-gsub("%tbl%",u,'SELECT DISTINCT(INST) as insts from %tbl%;')
 query_sent<-RMySQL::dbSendQuery(db,query_string)
 x<-RMySQL::dbFetch(query_sent)
 RMySQL::dbClearResult(query_sent)
 RMySQL::dbDisconnect(db)
+x$insts
 }
