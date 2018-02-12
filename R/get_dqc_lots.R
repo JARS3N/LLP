0ea09fbced00596dec3ff662da7c221f46febedf
JARS3N <JARS3N@users.noreply.github.com>
2018-02-12 11:36:55 -0500

    Update get_dqc_lots.R

--- R/get_dqc_lots.R
+++ R/get_dqc_lots.R
@@ -2,7 +2,7 @@ get_dqc_lots<-function(){
   db<-adminKraken::con_mysql()
   q<-"SELECT DISTINCT(Lot) from dryqcxf24;"
   send_query<-RMySQL::dbSendQuery(db,q)
-  fetch_query<-RMySQL::dbFetch(send_query,n=-1)
+  fetch_query<-RMySQL::dbFetch(send_query,n=Inf)
   RMySQL::dbClearResult(send_query)
   RMySQL::dbDisconnect(db)
   fetch_query
