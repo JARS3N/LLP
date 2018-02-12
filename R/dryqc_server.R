70719e01fa65c1d3a9040f085dc85ec9238a3a35
JARS3N <JARS3N@users.noreply.github.com>
2018-02-12 11:38:23 -0500

    Update dryqc_server.R

--- R/dryqc_server.R
+++ R/dryqc_server.R
@@ -2,11 +2,8 @@ dryqc_server <- function() {
   require(shiny)
   require(ggplot2)
   require(ggthemes)
-  
   shinyServer(function(input, output, session) {
-    updateSelectInput(session, 'Lot', choices = LLP::get_dqc_lots())
-    
-    
+    updateSelectInput(session, 'Lot', choices = rev(LLP::get_dqc_lots()))
     observeEvent(input$Lot, {
       DATA <- LLP::pull_dqc_data(input$Lot, input$analyte)
       output$plot1 <- renderPlot({
@@ -14,7 +11,8 @@ dryqc_server <- function() {
           geom_tufteboxplot(outlier.colour = "transparent") +
           theme_minimal() +
           theme(axis.text.x = element_text(angle = 90, hjust = 1)) +
-          ylab("%Transmission")
+          ylab("%Transmission")+
+          ggtitle(input$Lot)
       })
       #end
     })
