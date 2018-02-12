2ece956e0d2a56950b3426d133d235706e1c8f18
JARS3N <JARS3N@users.noreply.github.com>
2018-02-12 09:55:06 -0500

    Update pull_mvdata.R

--- R/pull_mvdata.R
+++ R/pull_mvdata.R
@@ -8,6 +8,7 @@
     RMySQL::dbDisconnect(db)
     fetch_query$use<-c("Production","Experimental")[as.numeric(factor(fetch_query$exp,levels=c(0,1)))]
    fetch_query$exp<-NULL
+  fetch_query<-arrange(fetch_query,yr,day)
    fetch_query$Lot<-factor(Lot,levels=unique(Lot))
     fetch_query
   }  
