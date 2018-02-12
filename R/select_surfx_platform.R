1323ad1157867127bbe5d705f1009653c3b1cc16
JARS3N <JARS3N@users.noreply.github.com>
2018-02-12 13:24:49 -0500

    Update select_surfx_platform.R

--- R/select_surfx_platform.R
+++ R/select_surfx_platform.R
@@ -1,12 +1,12 @@
 select_surfx_platform<-function(u){
 db<-adminKraken::con_mysql()
-q_str<-'SELECT CTG_LOT,RIGHT(CTG_LOT,2) as yr,SUBSTR(CTG_LOT,2,3) as day from
-(SELECT DISTINCT CTG_LOT from (SELECT CTG_LOT, LEFT(CTG_LOT,1) as ctype 
-FROM surfxmeta) as T1 where ctype="%type%")as T2
-order by yr DESC, day DESC;'
-send_query<-RMySQL::dbSendQuery(db,gsub("%type%",u,q_str))
+t1<-'(SELECT DISTINCT CTG_LOT from (SELECT CTG_LOT, LEFT(CTG_LOT,1) as ctype FROM surfxmeta) as T1 where ctype="%type%")'
+t2<-gsub("%t1%",t1,'SELECT CTG_LOT,RIGHT(CTG_LOT,2) as yr,SUBSTR(CTG_LOT,2,3) as day from %t1% as T2 order by yr DESC, day DESC;')
+send_query<-RMySQL::dbSendQuery(db,gsub("%type%",u,t2))
 fetch_query<-RMySQL::dbFetch(send_query)
 RMySQL::dbClearResult(send_query)
 RMySQL::dbDisconnect(db)
 fetch_query$CTG_LOT
 }
+
+        
