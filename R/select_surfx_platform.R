aa5a45c5cf4d57f94aaf250443aa4673ef378790
JARS3N <JARS3N@users.noreply.github.com>
2018-02-12 12:26:08 -0500

    Update select_surfx_platform.R

--- R/select_surfx_platform.R
+++ R/select_surfx_platform.R
@@ -4,7 +4,7 @@ q_str<-'SELECT CTG_LOT,RIGHT(CTG_LOT,2) as yr,SUBSTR(CTG_LOT,2,3) as day from
 (SELECT DISTINCT CTG_LOT from (SELECT CTG_LOT, LEFT(CTG_LOT,1) as ctype 
 FROM surfxmeta) as T1 where ctype="%type%")as T2
 order by yr DESC, day DESC;'
-send_query<-RMySQL::dbSendQuery(db,gsub("%type%",u,str))
+send_query<-RMySQL::dbSendQuery(db,gsub("%type%",u,q_str))
 fetch_query<-RMySQL::dbFetch(send_query)
 RMySQL::dbClearResult(send_query)
 RMySQL::dbDisconnect(db)
