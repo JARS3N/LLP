9b7cb77ec145c969b6c4af89251892c661a77c8e
JARS3N <JARS3N@users.noreply.github.com>
2018-02-12 09:42:31 -0500

    Update calc_cc_means.R

--- R/calc_cc_means.R
+++ R/calc_cc_means.R
@@ -1,7 +1,7 @@
 calc_cc_means<-function(tbl,var,inst){
 require(RMySQL)
 db<-adminKraken::con_mysql()
-T0<-gsub(%var%,var,'SELECT Lot,Inst,sn,%var% as var from xfpwetqc where Inst = "%inst%"')
+T0<-gsub("%var%",var,'SELECT Lot,Inst,sn,%var% as var from xfpwetqc where Inst = "%inst%"')
 T1<-gsub("%inst%",inst,T0)
 T2<-gsub("%T1%",T1,'SELECT Lot,sn,AVG(var) as iavg FROM (%T1%) as T1 group by Lot,sn')
 query_str<-gsub("%T2%",T2,'SELECT Lot,AVG(iavg)as avg,STD(iavg) as sd,RIGHT(Lot,2) as yr,substr(Lot,2,3) as day FROM (%T2%) as T2 group by Lot order by yr ASC,day ASC;')
