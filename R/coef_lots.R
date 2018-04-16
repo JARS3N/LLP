1903caad47a0da93ec6d7bb0b4e6ab6006796a98
JARS3N <JARS3N@users.noreply.github.com>
2018-04-16 10:49:12 -0400

    Update coef_lots.R

--- R/coef_lots.R
+++ R/coef_lots.R
@@ -2,7 +2,7 @@ coef_lots<-function(){
   require(RMySQL)
   db<-adminKraken::con_mysql()
   q1<-RMySQL::dbSendQuery(db,"Select CONCAT(Type, `Lot Number`) AS LotNumber,`Barcode Matrix ID` AS BMID from lotview;")
-  x<-RMySQL::dbFetch(q1)
+  x<-suppressWarnings(RMySQL::dbFetch(q1))
   RMySQL::dbClearResult(q1)
   RMySQL::dbDisconnect(db)
   return(x)
