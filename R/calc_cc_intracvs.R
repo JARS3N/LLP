c999e4a6319e7bebca167cf7bf12c377840b8915
JARS3N <JARS3N@users.noreply.github.com>
2018-09-17 16:08:51 -0400

    Update calc_cc_intracvs.R

--- R/calc_cc_intracvs.R
+++ R/calc_cc_intracvs.R
@@ -1,7 +1,7 @@
 calc_cc_intracvs<-function(tbl,var,inst,db){
 str_0<-"SELECT AVG(CV)as intraCV,stddev(CV) as intraCVsd,substr(Lot,4,2) as yr,substr(Lot,2,3) as day,Lot from
 (SELECT (100*(stddev(var)/AVG(var))) as CV,Lot from
-(SELECT Lot,sn,Inst,Gain as var from xfpwetqc where Inst=430029) as x
+(SELECT Lot,sn,Inst,`%VAR%` as var from xfpwetqc where Inst=430029) as x
 group by Lot,sn) as Y
 group by Lot
 ORDER by yr DESC,day DESC;"
