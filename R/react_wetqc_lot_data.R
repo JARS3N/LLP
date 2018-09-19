87892fd3c924979e5e7327e48d6e6a00a48d4845
JARS3N <JARS3N@users.noreply.github.com>
2018-09-19 12:31:30 -0400

    Update react_wetqc_lot_data.R

--- R/react_wetqc_lot_data.R
+++ R/react_wetqc_lot_data.R
@@ -1,8 +1,10 @@
 react_wetqc_lot_data<-function(platform,variable,inputLot,db){
-    q_string<-c("Select `",variable,"` as var,sn,Inst from ",platform," where Lot=",shQuote(inputLot),";")
-    q<-RMySQL::dbSendQuery(db,paste0(q_string,collapse=""))
-    a<-RMySQL::fetch(q)
-    RMySQL::dbClearResult(q)
-    a$Inst<-factor(a$Inst)
-    a
+  q_string <- c("Select `", variable, "` as var,sn,Inst from ", 
+                platform, " where Lot=", shQuote(inputLot), 
+                " AND `",variable,"` IS NOT NULL;")
+  q<-RMySQL::dbSendQuery(db,paste0(q_string,collapse=""))
+  a<-RMySQL::fetch(q)
+  RMySQL::dbClearResult(q)
+  a$Inst<-factor(a$Inst)
+  a
 }
