2c1666bc22ead0fde8c017a1276bb0cd4701b178
JARS3N <JARS3N@users.noreply.github.com>
2019-03-27 13:00:31 -0400

    Update wetqc_lot_view_server.R

--- R/wetqc_lot_view_server.R
+++ R/wetqc_lot_view_server.R
@@ -1,40 +1,55 @@
 
 wetqc_lot_view_server<-function(){
+aLOT <- LLP::lotstore$new()
 require(shiny)
 require(ggplot2)
 require(ggthemes)
 require(plotly)
+TBL <- new.env()
+TBL$B <- "xfe24wetqc"
+TBL$W <- "xfe96wetqc"
+TBL$C <- "xfpwetqc"
+TBL$Q <- "xf24legacy"
 
-# require(RMySQL)
-db<-adminKraken::con_mysql()
+db <- adminKraken::con_mysql()
 
-shinyServer(function(input, output,session) {
-    session$onSessionEnded(function() {
-        RMySQL::dbDisconnect(db)
-        stopApp()
-    })
-
-
-    selectedData <- reactive({
-        LLP::react_wetqc_lot_data(input$PLAT,input$Variable,input$Lot,db)
-    })
-
-
-    observeEvent(input$PLAT,{
-        updateSelectInput(session,'Lot',choices=LLP::unique_wetqc_lots(input$PLAT,db)$Lot)
-    }
-    )
-
-    output$plot1 <- renderPlotly({
-     ggplot(selectedData(),aes(x=factor(sn),y=var))+
-    geom_boxplot(outlier.shape = NA,outlier.alpha = 0.00001,outlier.color = 'white') +
-         geom_jitter(shape=22,alpha=.6,size=3.5,color="black",aes(fill=Inst))+
-     xlab("Serial Number") +
-         ylab(input$Variable)+
-       theme_bw() +
-         ggtitle(input$Lot)+
-        scale_fill_gdocs()
-    })
-
+shinyServer(function(input, output, session) {
+  session$onSessionEnded(function() {
+    RMySQL::dbDisconnect(db)
+    stopApp()
+  })
+  
+  
+  selectedData <- reactive({
+    LLP::react_wetqc_lot_data(get(input$PLAT, envir = TBL), input$Variable, input$Lot, db)
+  })
+  
+  
+  observeEvent(input$PLAT, {
+    updateSelectInput(session, 'Lot', choices = aLOT$get(input$PLAT))
+  })
+  
+  output$plot1 <- renderPlotly({
+    ggplot(selectedData(), aes(x = factor(sn), y = var)) +
+      geom_boxplot(
+        outlier.shape = NA,
+        outlier.alpha = 0.00001,
+        outlier.color = 'white'
+      ) +
+      geom_jitter(
+        shape = 22,
+        alpha = .6,
+        size = 3.5,
+        color = "black",
+        aes(fill = Inst)
+      ) +
+      xlab("Serial Number") +
+      ylab(input$Variable) +
+      theme_bw() +
+      ggtitle(input$Lot) +
+      scale_fill_gdocs()
+  })
+  
 })
+
 }
