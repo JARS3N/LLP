ed8c30260252bc48b6044aa0d8d4c440e994eec5
JARS3N <JARS3N@users.noreply.github.com>
2019-03-27 12:34:28 -0400

    Update qcnotes_ui.R

--- R/qcnotes_ui.R
+++ R/qcnotes_ui.R
@@ -1,59 +1,60 @@
 qcnotes_ui <- function() {
-  library(shiny)
-  shinyUI(fluidPage(title = 'QCLOTNOTES',
-                    sidebarLayout(
-                      sidebarPanel(
-                        helpText(),
-                        selectInput(
-                          "PLAT",
-                          "Platform",
-                          c(
-                            "XF24" = "XF24legacy",
-                            "XFe24" = "XFe24wetqc",
-                            "XFe96" = "XFe96wetqc",
-                            "XFp" = "XFpwetqc"
-                          ),
-                          selected = FALSE,
-                          multiple = FALSE
+library(shiny)
+shinyUI(fluidPage(title = 'QCLOTNOTES',
+                  sidebarLayout(
+                    sidebarPanel(
+                      helpText(),
+                      selectInput(
+                        "PLAT",
+                        "Platform",
+                        c(
+                          "XF24" = "Q",
+                          "XFe24" = "B",
+                          "XFe96" = "W",
+                          "XFp" = "C"
                         ),
-                        selectInput(
-                          "Lot",
-                          "Select Lot",
-                          "NULL" ,
-                          selected = FALSE,
-                          multiple = FALSE
-                        ),
-                        actionButton('button', "Enter Data", icon = NULL, width = NULL)
+                        selected = FALSE,
+                        multiple = FALSE
                       ),
-                      mainPanel(
-                        h1("Add Notes"),
-                        
-                        textInput(
-                          'txtnotes1',
-                          "Notes",
-                          value = "",
-                          width = '100%',
-                          placeholder = 'Notes,each entry can be 255 characters in length.
+                      selectInput(
+                        "Lot",
+                        "Select Lot",
+                        "NULL" ,
+                        selected = FALSE,
+                        multiple = FALSE
+                      ),
+                      actionButton('button', "Enter Data", icon = NULL, width = NULL)
+                    ),
+                    mainPanel(
+                      h1("Add Notes"),
+                      
+                      textInput(
+                        'txtnotes1',
+                        "Notes",
+                        value = "",
+                        width = '100%',
+                        placeholder = 'Notes,each entry can be 255 characters in length.
                           If you need to add more enter another line.'
-                        ),
-                        textInput(
-                          'txtnotes2',
-                          "",
-                          value = "",
-                          width = '100%',
-                          placeholder = ''
-                        ),
-                        textInput(
-                          'txtnotes3',
-                          "",
-                          value = "",
-                          width = '100%',
-                          placeholder = ''
-                        ),
-                        h2("Notes in Database:"),
-                        p(textOutput(outputId = "text1"))
-                        
-                        
-                        )
-  )))
+                      ),
+                      textInput(
+                        'txtnotes2',
+                        "",
+                        value = "",
+                        width = '100%',
+                        placeholder = ''
+                      ),
+                      textInput(
+                        'txtnotes3',
+                        "",
+                        value = "",
+                        width = '100%',
+                        placeholder = ''
+                      ),
+                      h2("Notes in Database:"),
+                      p(textOutput(outputId = "text1"))
+                      
+                      
+                    )
+                  )))
+
 }
