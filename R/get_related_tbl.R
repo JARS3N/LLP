2b933bee8c82d2a645d2ebd9ef0ad7eb7a31fc6e
JARS3N <JARS3N@users.noreply.github.com>
2019-05-01 11:58:11 -0400

    Create get_related_tbl.R

---
+++ R/get_related_tbl.R
@@ -0,0 +1,44 @@
+get_related_tbl <- function(LOT, TYPE) {
+  if (LOT == "") {
+    return(NULL)
+  }
+  if (is.null(LOT)) {
+    return(NULL)
+  }
+  checkforlot <-
+    tbl(db, "lotview") %>% 
+    select(`Lot Number`) %>% 
+    distinct() %>% 
+    filter(`Lot Number` == LOT) %>% 
+    count() %>% 
+    collect()
+  
+  if (checkforlot$n < 1) {
+    return(NULL)
+  }
+  out <- tbl(db, "lotview") %>%
+    filter(Type == TYPE, `Lot Number` == LOT) %>%
+    select(Lot = `Lot Number`, Type, ID = `Barcode Matrix ID`) %>%
+    collect()
+  print(out)
+  if (is.null(out)) {
+    return(NULL)
+  }
+  out2 <- tbl(db, "barcodematrixview") %>%
+    filter(ID == out$ID[1]) %>%
+    select(MF = `Multifluor`,
+           pH = `pH Fluor`,
+           O2 = `Ox Fluor`) %>%
+    collect()
+  print(out2)
+  out3 <-
+    tbl(db, "lotview") %>%
+    filter(`Barcode Matrix ID` == out$ID & Type == TYPE) %>%
+    select(Lot = `Lot Number`) %>%
+    distinct() %>%
+    collect() %>%
+    mutate(Lot = paste0(TYPE, Lot))
+  
+  return(merge(out3, out2))
+  
+}
