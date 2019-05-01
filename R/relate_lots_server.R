b08d0181ab8307ae649811211914f0713d76ee2e
JARS3N <JARS3N@users.noreply.github.com>
2019-05-01 12:03:22 -0400

    Update relate_lots_server.R

--- R/relate_lots_server.R
+++ R/relate_lots_server.R
@@ -2,7 +2,7 @@ related_lots_server <- function() {
     library(shiny)
     library(dplyr)
     db <- adminKraken::con_dplyr()
-    shinyServer(function(input, output, server) {
+    shinyServer(function(input, output, session) {
         observeEvent(input$search, {
             mfltbl <- get_related_tbl(input$Lot, input$ltype)
             
