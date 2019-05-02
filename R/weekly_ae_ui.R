6778a7dac44658913e3862ad143e5ecafd0d0de1
JARS3N <JARS3N@users.noreply.github.com>
2019-05-02 10:39:50 -0400

    Create weekly_ae_ui.R

---
+++ R/weekly_ae_ui.R
@@ -0,0 +1,14 @@
+weekly_ae_ui<-function(){
+library(shiny)
+
+shinyUI(fluidPage(
+    titlePanel("Applied Engineering Weekly Breakdown"),
+        mainPanel(
+            p('%Time per Project'),
+            h4(textOutput("weekchosen")),
+            dateInput('dates','dates'),
+            DTOutput('testDT') 
+        )
+    
+))
+}
