ee7d456050c976e8e3cd9c742274a57bc2f31e7b
JARS3N <JARS3N@users.noreply.github.com>
2019-05-16 12:19:10 -0400

    Create ol_ui.R

---
+++ R/ol_ui.R
@@ -0,0 +1,29 @@
+ol_ui<-function(){
+  library(shiny)
+  shinyUI(fluidPage(
+    titlePanel("Outliers"),
+    sidebarLayout(
+      sidebarPanel(
+        width = 3,
+        textInput(
+          'exp',
+          'Experiment',
+          value = "",
+          width = NULL,
+          placeholder = NULL
+        ),
+        fileInput("file", label = h3("File input"), multiple = T),
+        radioButtons("group", "Grouping:",
+                     c("All", "byFile")),
+        radioButtons("ptype", "Plot Type:",
+                     c("Density", "Bar")),
+        radioButtons("copy_asyrs", "include asyr files in export:",
+                     c("Yes", "No")),
+        downloadButton('export', "Export as Zip")
+
+      ),
+      mainPanel(plotOutput("olplot"),
+                dataTableOutput('table'))
+    )
+  ))
+}
