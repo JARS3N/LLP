8708eb400ae04bb16100b316dd4a135c64dcde3e
JARS3N <JARS3N@users.noreply.github.com>
2019-05-31 11:11:57 -0400

    Create project_hours_check_date.R

---
+++ R/project_hours_check_date.R
@@ -0,0 +1,11 @@
+check_date<-function(date,db){
+require(dplyr)
+    yr<-lubridate::year(date)
+    mnth<-lubridate::month(date)
+    wk<-lubridate::week(date)
+    tbl(db,"projecthours") %>% 
+        filter(year==yr,week==wk) %>% 
+        count() %>% 
+        collect() %>% 
+        .$n != 0
+}
