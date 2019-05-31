24970a5939c86815298590ce91a0c2f0026baadb
JARS3N <JARS3N@users.noreply.github.com>
2019-05-31 11:10:14 -0400

    Create project_hours_new_data_set.R

---
+++ R/project_hours_new_data_set.R
@@ -0,0 +1,16 @@
+new_data_set<-function(date,db){
+require(dplyr)
+    #db<-adminKraken::con_dplyr()
+    A<-tbl(db,"projecttasks") %>% 
+        filter(active==1) %>% 
+        collect()
+    B<-tbl(db,"projectusers") %>% 
+        filter(active==1) %>% 
+        collect()
+    X<-mutate(dplyr::bind_rows(lapply(B$user,function(u){mutate(A,user=u)})),hours=0)
+    X$year<-lubridate::year(date)
+    X$month<-lubridate::month(date)
+    X$week<-lubridate::week(date)
+    X$active<-NULL
+    X
+}
