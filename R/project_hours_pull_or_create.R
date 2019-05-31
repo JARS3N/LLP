d67010ab0d2ac8a05a1fea5ac1568be902db7c1e
JARS3N <JARS3N@users.noreply.github.com>
2019-05-31 11:13:50 -0400

    Create project_hours_pull_or_create.R

---
+++ R/project_hours_pull_or_create.R
@@ -0,0 +1,25 @@
+pull_or_create <- function(date, db) {
+require(dplyr)
+    ck_date <- check_date(date, db)
+    print(ck_date)
+    if (ck_date == FALSE) {
+        message("starting new date block")
+        tmp <-  new_data_set(date, db)
+        db2 <- adminKraken::con_mysql()
+        RMySQL::dbWriteTable(
+            db2,
+            "projecthours",
+            tmp,
+            overwrite = FALSE,
+            append = TRUE,
+            row.names = FALSE
+        )
+        dbDisconnect(db2)
+    }
+    yr<-lubridate::year(date)
+    wk<-lubridate::week(date)
+    tbl(db,"projecthours") %>% 
+        filter(year==yr,week==wk) %>% 
+        collect() %>% tidyr::spread(., user, hours) %>%
+        select(-week,-month,-year)
+}
