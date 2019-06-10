03632f0229fe0dbb7e7d16450b9a1cda7fd0aeb6
JARS3N <JARS3N@users.noreply.github.com>
2019-06-10 11:42:37 -0400

    Update project_hours_check_date.R

--- R/project_hours_check_date.R
+++ R/project_hours_check_date.R
@@ -2,7 +2,7 @@ check_date<-function(date,db){
 require(dplyr)
     yr<-lubridate::year(date)
     mnth<-lubridate::month(date)
-    wk<-lubridate::week(date)
+    wk<-lubridate::isoweek(date)
     tbl(db,"projecthours") %>% 
         filter(year==yr,week==wk) %>% 
         count() %>% 
