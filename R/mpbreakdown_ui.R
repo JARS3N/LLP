1b0f51e6b7282e5645934782b1fc1af5fa08b090
JARS3N <JARS3N@users.noreply.github.com>
2019-07-16 11:15:10 -0400

    Update mpbreakdown_ui.R

--- R/mpbreakdown_ui.R
+++ R/mpbreakdown_ui.R
@@ -2,12 +2,24 @@ mpbreakdown_ui<-function(){
 library(shiny)
 shinyUI(fluidPage(
   titlePanel("Applied Eng. Monthly Project Breakdown"),
-  sidebarLayout(
-    mainPanel(plotOutput("distPlot")),
-    sidebarPanel(
-      selectInput("year", "year", choices = "2019"),
-      selectInput("month", "month", choices = 6)
-    )
+  fluidRow(
+    column(width = 6, selectInput("year",
+                                  "year", choices = "2019")),
+    column(
+      width = 6,
+      selectInput(
+        inputId = "month",
+        label = "month",
+        choices = "6"
+      )
+    ),
+    fluidRow(column(width = 6,
+                    plotOutput("distPlot"))
+             ,
+             column(
+               width = 6,
+               plotOutput("distPlot2", width = "100%")
+             ))
   )
 ))
 }
