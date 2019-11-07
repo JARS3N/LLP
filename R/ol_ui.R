360ca93d77c705fd37cae51cccdfef5f1991e206
JARS3N <JARS3N@users.noreply.github.com>
2019-11-07 14:21:41 -0500

    Update ol_ui.R

--- R/ol_ui.R
+++ R/ol_ui.R
@@ -1,8 +1,5 @@
 ol_ui<-function(){
   library(shiny)
-  #shinyUI(fluidPage( tags$head(tags$style(HTML( ".dataTables_scroll      {overflow-x:scroll;}"))),
-  #tags$head(tags$style(HTML( ".dataTables_scrollBody  {overflow: unset !important;}"))),
-  #tags$head(tags$style(HTML( ".dataTables_scrollHead  {overflow: unset !important; z-index: 10;}"))),
     titlePanel("Outliers"),
     sidebarLayout(
       sidebarPanel(
