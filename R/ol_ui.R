88183fe9e7c011c9ffadee3cfc2f04db406550a8
JARS3N <JARS3N@users.noreply.github.com>
2019-11-07 14:25:09 -0500

    Update ol_ui.R

--- R/ol_ui.R
+++ R/ol_ui.R
@@ -1,29 +1,30 @@
 ol_ui<-function(){
-  library(shiny)
-    titlePanel("Outliers")
-    sidebarLayout(
-      sidebarPanel(
-        width = 3,
-        textInput(
-          'exp',
-          'Experiment',
-          value = "",
-          width = NULL,
-          placeholder = NULL
-        ),
-        fileInput("file", label = h3("File input"), multiple = T),
-        radioButtons("group", "Grouping:",
-                     c("All", "byFile")),
-        radioButtons("ptype", "Plot Type:",
-                     c("Density", "Bar")),
-        radioButtons("copy_asyrs", "include asyr files in export:",
-                     c("Yes", "No")),
-        downloadButton('export', "Export as Zip")
-
+library(shiny)
+shinyUI(fluidPage(
+  titlePanel("Outliers"),
+  sidebarLayout(
+    sidebarPanel(
+      width = 3,
+      textInput(
+        'exp',
+        'Experiment',
+        value = "",
+        width = NULL,
+        placeholder = NULL
       ),
-      mainPanel(plotOutput("olplot"),
-             #   dataTableOutput('table')
-               )
-    )
-  ))
+      fileInput("file", label = h3("File input"), multiple = T),
+      radioButtons("group", "Grouping:",
+                   c("All", "byFile")),
+      radioButtons("ptype", "Plot Type:",
+                   c("Density", "Bar")),
+      radioButtons("copy_asyrs", "include asyr files in export:",
+                   c("Yes", "No")),
+      downloadButton('export', "Export as Zip")
+      
+    ),
+    mainPanel(plotOutput("olplot")
+              #,dataTableOutput('table')
+              )
+  )
+))
 }
