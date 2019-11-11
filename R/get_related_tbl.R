b7693e70b4103c84b93ee0cc864ff9d66736d361
JARS3N <JARS3N@users.noreply.github.com>
2019-11-11 10:45:38 -0500

    Update get_related_tbl.R

--- R/get_related_tbl.R
+++ R/get_related_tbl.R
@@ -1,44 +1,31 @@
-get_related_tbl <- function(LOT, TYPE,db) {
+get_related_tbl<-function (LOT, TYPE, db) 
+{
   if (LOT == "") {
     return(NULL)
   }
   if (is.null(LOT)) {
     return(NULL)
   }
- checkforlot <-
-    tbl(db, "lotview") %>% 
-    select(`Lot Number`,Type) %>% 
-    distinct() %>% 
-    filter(`Lot Number` == LOT & Type == TYPE) %>% 
-    count() %>% 
-    collect()
-  
+  checkforlot <- tbl(db, "lotview") %>% select(`Lot Number`, 
+                                               Type) %>% distinct() %>% filter(`Lot Number` == LOT & 
+                                                                                 Type == TYPE) %>% count() %>% collect()
   if (checkforlot$n < 1) {
     return(NULL)
   }
-  out <- tbl(db, "lotview") %>%
-    filter(Type == TYPE, `Lot Number` == LOT) %>%
-    select(Lot = `Lot Number`, Type, ID = `Barcode Matrix ID`) %>%
-    collect()
-  print(out)
+  out <- tbl(db, "lotview") %>% filter(Type == TYPE, `Lot Number` == 
+                                         LOT) %>% select(Lot = `Lot Number`, Type, ID = `Barcode Matrix ID`) 
+  
   if (is.null(out)) {
     return(NULL)
   }
-  out2 <- tbl(db, "barcodematrixview") %>%
-    filter(ID == out$ID[1]) %>%
-    select(MF = `Multifluor`,
-           pH = `pH Fluor`,
-           O2 = `Ox Fluor`) %>%
+  out3<-left_join(out,tbl(db, "barcodematrixview")) %>% 
+    select(MF = Multifluor, pH = `pH Fluor`, O2 = `Ox Fluor`,`Barcode Matrix ID`=ID) %>% 
+    left_join(.,tbl(db, "lotview")) %>% 
+    mutate(Lot=paste0(Type,`Lot Number`)) %>% 
+    select(Lot,MF,pH,O2) %>% 
+    distinct() %>% 
     collect()
-  print(out2)
-  out3 <-
-    tbl(db, "lotview") %>%
-    filter(`Barcode Matrix ID` == out$ID & Type == TYPE) %>%
-    select(Lot = `Lot Number`) %>%
-    distinct() %>%
-    collect() %>%
-    mutate(Lot = paste0(TYPE, Lot))
   
-  return(merge(out3, out2))
   
+  return(out3)
 }
