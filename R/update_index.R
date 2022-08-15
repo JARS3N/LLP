c0781bd3603e6e0d2bf98cd6be383e6a918240d4
JARS3N <JARS3N@users.noreply.github.com>
2022-08-15 11:15:39 -0400

    Update update_index.R

--- R/update_index.R
+++ R/update_index.R
@@ -1,4 +1,5 @@
 update_index<-function(){
+  rmarkdown::render(list.files(system.file(package="LLP"),pattern='index.rmd',full.names=T))
   from<-list.files(system.file(package="LLP"),pattern='index.html',full.names=T)
   to<-"/home/jarsenault/ShinyApps/index.html"
   old <-"/home/jarsenault/ShinyApps/index.html.old"
