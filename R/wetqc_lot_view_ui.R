f6985534b389a0fbcb5051e551a768a958975d51
JARS3N <JARS3N@users.noreply.github.com>
2025-03-17 12:21:07 -0400

    Update wetqc_lot_view_ui.R

--- R/wetqc_lot_view_ui.R
+++ R/wetqc_lot_view_ui.R
@@ -1,46 +1,22 @@
-wetqc_lot_view_ui<-function(){
-require(shiny)
-require(plotly)
-shinyUI(fluidPage(
+wetqc_lot_view_ui <- function() {
+  fluidPage(
     headerPanel("Lot QC Data"),
-    mainPanel(plotlyOutput('plot1')),
-    sidebarPanel(
+    sidebarLayout(
+      sidebarPanel(
         selectInput(
-            "PLAT",
-            "Platform",
-        c(
-          "XFe24" = "B",
-          "XFe96" = "W",
-          "XFp" = "C",
-          "XF24" = "Q"
+          "PLAT", "Platform",
+          c("XFe24" = "B", "XFe96" = "W", "XFp" = "C"),
+          selected = NULL, multiple = FALSE
         ),
-            selected = FALSE,
-            multiple = FALSE
-        ),
+        selectInput("Lot", "Select Lot", c('Lots'), selected = NULL, multiple = FALSE),
         selectInput(
-            "Lot",
-            "Select Lot",
-            c('Lots'),
-            selected = FALSE,
-            multiple = FALSE
-        ),
-        selectInput(
-            "Variable",
-            "Select Variable",
-            c(
-                "O2.LED",
-                "O2.CalEmission",
-                "KSV",
-                "Ambient",
-                "F0",
-                "pH.LED",
-                "pH.CalEmission",
-                "Gain",
-                "sorpH"
-            ),
-            multiple = FALSE
+          "Variable", "Select Variable",
+          c("O2.LED", "O2.CalEmission", "KSV", "Ambient", "F0",
+            "pH.LED", "pH.CalEmission", "Gain", "sorpH"),
+          selected = NULL, multiple = FALSE
         )
-        
+      ),
+      mainPanel(plotlyOutput('plot1'))
     )
-))
+  )
 }
