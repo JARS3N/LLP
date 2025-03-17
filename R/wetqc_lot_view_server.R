c9c999aa52c1a76fdd1ee735f382c66ad2d40c44
JARS3N <JARS3N@users.noreply.github.com>
2025-03-17 12:21:58 -0400

    Update wetqc_lot_view_server.R

--- R/wetqc_lot_view_server.R
+++ R/wetqc_lot_view_server.R
@@ -1,55 +1,46 @@
+wetqc_lot_view_server <- function(input, output, session) {
+  aLOT <- LLP::lotstore$new()
 
-wetqc_lot_view_server<-function(){
-aLOT <- LLP::lotstore$new()
-require(shiny)
-require(ggplot2)
-require(ggthemes)
-require(plotly)
-TBL <- new.env()
-TBL$B <- "xfe24wetqc"
-TBL$W <- "xfe96wetqc"
-TBL$C <- "xfpwetqc"
-TBL$Q <- "xf24legacy"
+  # Use reactiveVal for table storage
+  TBL <- reactiveVal(list(
+    B = "xfe24wetqc",
+    W = "xfe96wetqc",
+    C = "xfpwetqc"
+  ))
 
-db <- adminKraken::con_mysql()
+  # Establish database connection
+  db <- connect_db()
 
-shinyServer(function(input, output, session) {
+  # Close database connection when session ends
   session$onSessionEnded(function() {
-    RMySQL::dbDisconnect(db)
+    if (!is.null(db)) RMySQL::dbDisconnect(db)
     stopApp()
   })
-  
-  
+
+  # Reactive expression for data selection
   selectedData <- reactive({
-    LLP::react_wetqc_lot_data(get(input$PLAT, envir = TBL), input$Variable, input$Lot, db)
+    LLP::react_wetqc_lot_data(get(input$PLAT, envir = TBL()), input$Variable, input$Lot, db)
   })
-  
-  
+
+  # Observe platform selection to update lot choices
   observeEvent(input$PLAT, {
     updateSelectInput(session, 'Lot', choices = aLOT$get(input$PLAT))
   })
-  
+
+  # Cache plot data
+  plot_data <- reactive({
+    selectedData()
+  })
+
+  # Render Plotly plot
   output$plot1 <- renderPlotly({
-    ggplot(selectedData(), aes(x = factor(sn), y = var)) +
-      geom_boxplot(
-        outlier.shape = NA,
-        outlier.alpha = 0.00001,
-        outlier.color = 'white'
-      ) +
-      geom_jitter(
-        shape = 22,
-        alpha = .6,
-        size = 3.5,
-        color = "black",
-        aes(fill = Inst)
-      ) +
+    ggplot(plot_data(), aes(x = factor(sn), y = var)) +
+      geom_boxplot(outlier.shape = NA, outlier.alpha = 0.00001, outlier.color = 'white') +
+      geom_jitter(shape = 22, alpha = .6, size = 3.5, color = "black", aes(fill = Inst)) +
       xlab("Serial Number") +
       ylab(input$Variable) +
       theme_bw() +
       ggtitle(input$Lot) +
       scale_fill_gdocs()
   })
-  
-})
-
 }
