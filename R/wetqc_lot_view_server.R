692a18899b0a41c1480e6db37f64753ea4c32d2c
JARS3N <JARS3N@users.noreply.github.com>
2025-03-17 12:24:09 -0400

    Update wetqc_lot_view_server.R

--- R/wetqc_lot_view_server.R
+++ R/wetqc_lot_view_server.R
@@ -2,45 +2,45 @@ wetqc_lot_view_server <- function(input, output, session) {
   aLOT <- LLP::lotstore$new()
 
   # Use reactiveVal for table storage
-  TBL <- reactiveVal(list(
+  TBL <- shiny::reactiveVal(list(
     B = "xfe24wetqc",
     W = "xfe96wetqc",
     C = "xfpwetqc"
   ))
 
-  # Establish database connection
-  db <- connect_db()
+  # Database connection with error handling
+  db <- tryCatch(
+    adminKraken::con_mysql(),
+    error = function(e) {
+      message("Database connection failed: ", e)
+      NULL
+    }
+  )
+  if (is.null(db)) stop("Could not connect to the database.")
 
   # Close database connection when session ends
   session$onSessionEnded(function() {
     if (!is.null(db)) RMySQL::dbDisconnect(db)
-    stopApp()
+    shiny::stopApp()
   })
 
-  # Reactive expression for data selection
-  selectedData <- reactive({
+  # Reactive Data Selection
+  selectedData <- shiny::reactive({
     LLP::react_wetqc_lot_data(get(input$PLAT, envir = TBL()), input$Variable, input$Lot, db)
   })
 
-  # Observe platform selection to update lot choices
-  observeEvent(input$PLAT, {
-    updateSelectInput(session, 'Lot', choices = aLOT$get(input$PLAT))
+  shiny::observeEvent(input$PLAT, {
+    shiny::updateSelectInput(session, 'Lot', choices = aLOT$get(input$PLAT))
   })
 
-  # Cache plot data
-  plot_data <- reactive({
-    selectedData()
-  })
-
-  # Render Plotly plot
-  output$plot1 <- renderPlotly({
-    ggplot(plot_data(), aes(x = factor(sn), y = var)) +
-      geom_boxplot(outlier.shape = NA, outlier.alpha = 0.00001, outlier.color = 'white') +
-      geom_jitter(shape = 22, alpha = .6, size = 3.5, color = "black", aes(fill = Inst)) +
-      xlab("Serial Number") +
-      ylab(input$Variable) +
-      theme_bw() +
-      ggtitle(input$Lot) +
-      scale_fill_gdocs()
+  output$plot1 <- plotly::renderPlotly({
+    ggplot2::ggplot(selectedData(), ggplot2::aes(x = factor(sn), y = var)) +
+      ggplot2::geom_boxplot(outlier.shape = NA, outlier.alpha = 0.00001, outlier.color = 'white') +
+      ggplot2::geom_jitter(shape = 22, alpha = .6, size = 3.5, color = "black", ggplot2::aes(fill = Inst)) +
+      ggplot2::xlab("Serial Number") +
+      ggplot2::ylab(input$Variable) +
+      ggplot2::theme_bw() +
+      ggplot2::ggtitle(input$Lot) +
+      ggthemes::scale_fill_gdocs()
   })
 }
