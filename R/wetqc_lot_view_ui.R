bde765701e6050942a8f92fdd68b00cf4f743583
JARS3N <JARS3N@users.noreply.github.com>
2025-03-17 12:24:39 -0400

    Update wetqc_lot_view_ui.R

--- R/wetqc_lot_view_ui.R
+++ R/wetqc_lot_view_ui.R
@@ -1,22 +1,22 @@
 wetqc_lot_view_ui <- function() {
-  fluidPage(
-    headerPanel("Lot QC Data"),
-    sidebarLayout(
-      sidebarPanel(
-        selectInput(
+  shiny::fluidPage(
+    shiny::headerPanel("Lot QC Data"),
+    shiny::sidebarLayout(
+      shiny::sidebarPanel(
+        shiny::selectInput(
           "PLAT", "Platform",
           c("XFe24" = "B", "XFe96" = "W", "XFp" = "C"),
           selected = NULL, multiple = FALSE
         ),
-        selectInput("Lot", "Select Lot", c('Lots'), selected = NULL, multiple = FALSE),
-        selectInput(
+        shiny::selectInput("Lot", "Select Lot", c('Lots'), selected = NULL, multiple = FALSE),
+        shiny::selectInput(
           "Variable", "Select Variable",
           c("O2.LED", "O2.CalEmission", "KSV", "Ambient", "F0",
             "pH.LED", "pH.CalEmission", "Gain", "sorpH"),
           selected = NULL, multiple = FALSE
         )
       ),
-      mainPanel(plotlyOutput('plot1'))
+      shiny::mainPanel(plotly::plotlyOutput('plot1'))
     )
   )
 }
