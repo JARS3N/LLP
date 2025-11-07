94e795908c50a71a3fd6e103a20e03c62aff0780
JARS3N <JARS3N@users.noreply.github.com>
2025-11-07 12:38:52 -0500

    Improve plot rendering with data validation
    Refactor plot rendering to ensure data safety and uniqueness.

--- R/wetqc_lot_view_server.R
+++ R/wetqc_lot_view_server.R
@@ -30,16 +30,29 @@ wetqc_lot_view_server <- function(input, output, session) {
     LLP::react_wetqc_lot_data(tbl_name, input$Variable, input$Lot, db)
   })
 
-  output$plot1 <- plotly::renderPlotly({
-    dat <- selectedData()
-    ggplot2::ggplot(dat, ggplot2::aes(x = factor(sn), y = val)) +
-      ggplot2::geom_boxplot(outlier.shape = NA, outlier.alpha = 1e-5, outlier.color = "white") +
-      ggplot2::geom_jitter(shape = 22, alpha = .6, size = 3.5, color = "black",
-                           ggplot2::aes(fill = Inst)) +
-      ggplot2::xlab("Serial Number") +
-      ggplot2::ylab(input$Variable) +
-      ggplot2::theme_bw() +
-      ggplot2::ggtitle(input$Lot) +
-      ggthemes::scale_fill_gdocs()
-  })
+output$plot1 <- plotly::renderPlotly({
+  dat <- selectedData()
+  shiny::req(NROW(dat) > 0)
+
+  dat <- as.data.frame(dat)
+  # make sure names are safe/unique (prevents accidental collisions)
+  names(dat) <- make.names(names(dat), unique = TRUE)
+
+  # factors for grouping
+  dat$sn   <- factor(dat$sn)
+  dat$Inst <- factor(dat$Inst)
+
+  p <- ggplot2::ggplot(dat, ggplot2::aes(x = sn, y = val, fill = Inst)) +
+    ggplot2::geom_boxplot(outlier.shape = NA, outlier.alpha = 1e-5, outlier.color = "white") +
+    ggplot2::geom_jitter(shape = 22, alpha = .6, size = 3.5, color = "black",
+                         width = 0.2, height = 0) +
+    ggplot2::xlab("Serial Number") +
+    ggplot2::ylab(input$Variable) +
+    ggplot2::ggtitle(input$Lot) +
+    ggplot2::theme_bw() +
+    ggthemes::scale_fill_gdocs()
+
+  # KEY FIX: avoid ggplotly's internal merge that causes `fix.by`
+  plotly::ggplotly(p, originalData = FALSE, tooltip = c("sn","val","Inst"))
+})
 }
